420ab87a5719e14f36b54d8f07a4e01f09243b1c
Andreo <39822719+andreoy@users.noreply.github.com>
2025-01-13 10:33:04 +0700

    Add files via upload

---
+++ precache-manifest.b00b564b40615b5561ad8c229029a110.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f374a877aea9bbeba59362be4ae9483e",
+    "url": "/osj/index.html"
+  },
+  {
+    "revision": "313069482d7bde9d842f",
+    "url": "/osj/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "6da06725a51acd908060",
+    "url": "/osj/static/css/main.3522ec90.chunk.css"
+  },
+  {
+    "revision": "313069482d7bde9d842f",
+    "url": "/osj/static/js/2.4a2773d6.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/osj/static/js/2.4a2773d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89dd3b6d840be2459e4",
+    "url": "/osj/static/js/3.9c4042b1.chunk.js"
+  },
+  {
+    "revision": "6da06725a51acd908060",
+    "url": "/osj/static/js/main.d30861a1.chunk.js"
+  },
+  {
+    "revision": "634c80a4a9c109a2ad43",
+    "url": "/osj/static/js/runtime-main.2a916523.js"
+  }
+]);
